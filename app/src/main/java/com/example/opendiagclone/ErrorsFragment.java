bfa4b147662e027bee833755cf47634520c95d60
IGAprod <34273952+IGAprod@users.noreply.github.com>
2019-07-06 23:30:36 +0500

    addMenuActivity
    addMenuActivity

---
+++ app/src/main/java/com/example/opendiagclone/ErrorsFragment.java
@@ -0,0 +1,20 @@
+package com.example.opendiagclone;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ErrorsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_errors,container,false);
+    }
+
+}
--- app/src/main/java/com/example/opendiagclone/MainActivity.java
+++ app/src/main/java/com/example/opendiagclone/MainActivity.java
@@ -3,33 +3,50 @@ package com.example.opendiagclone;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private TabLayout tabLayoutType;
+    private TabLayout tabLayoutMark;
+    private ViewPager viewPagerType;
+    private ViewPager viewPagerMark;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_information);
-                    return true;
+                    selectedFragment = new InfoFragment();
+                    break;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_parametrs);
-                    return true;
+                    selectedFragment = new ParametersFragment();
+                    break;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_errors);
-                    return true;
+                    selectedFragment = new ErrorsFragment();
+                    break;
             }
-            return false;
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+            return true;
         }
     };
 
@@ -40,6 +57,67 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new InfoFragment()).commit();
+
+      //  viewPagerType = (ViewPager) findViewById(R.id.viewPagerType);
+        tabLayoutType = findViewById(R.id.tabLayoutType);
+        tabLayoutMark = findViewById(R.id.tabLayoutMark);
+
+        tabLayoutType.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.up_right_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                mTextMessage.setText(R.string.title_information);
+                selectedFragment = new InfoFragment();
+                break;
+            case R.id.navigation_dashboard:
+                mTextMessage.setText(R.string.title_parametrs);
+                selectedFragment = new ParametersFragment();
+                break;
+            case R.id.navigation_notifications:
+                mTextMessage.setText(R.string.title_errors);
+                selectedFragment = new ErrorsFragment();
+                break;
+            case R.id.add_block:
+                mTextMessage.setText(R.string.add_block);
+                selectedFragment = new AddBlockFragment();
+                break;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                selectedFragment).commit();
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
