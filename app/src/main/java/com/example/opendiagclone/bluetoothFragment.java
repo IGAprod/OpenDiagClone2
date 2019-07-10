e2ff1556d89c109145b5816edbca3c79b800ee02
IGAprod <34273952+IGAprod@users.noreply.github.com>
2019-07-10 14:47:02 +0500

    FixBluetoothPermission
    FixBluetoothPermission

--- app/src/main/java/com/example/opendiagclone/bluetoothFragment.java
+++ app/src/main/java/com/example/opendiagclone/bluetoothFragment.java
@@ -17,22 +17,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
-
 import com.example.opendiagclone.adapter.BluetoothListAdapter;
 import com.example.opendiagclone.models.Device;
-
-
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Set;
+
 
 public class bluetoothFragment extends Fragment {
 
@@ -47,34 +43,32 @@ public class bluetoothFragment extends Fragment {
     private final BroadcastReceiver mReceiver=new BroadcastReceiver(){
         @Override
         public void onReceive(Context context, Intent intent){
-            Log.d(TAG, "ЗДЕСЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬЬ");
             String action= intent.getAction();
             Log.d(TAG, action);
             Toast.makeText(getActivity(),
                     action, Toast.LENGTH_LONG).show();
             if(BluetoothDevice.ACTION_FOUND.equals(action)){
-                Log.d(TAG, "ТАМММММММММММММММММММММММММММММММММММММММММММММММММММММММММММММ");
+                Log.d(TAG, "ACTION_FOUND");
                 BluetoothDevice device= intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
                     Device nearDevice = new Device(device.getName(), device.getAddress());
                     Toast.makeText(getActivity(),
                             nearDevice.toString(), Toast.LENGTH_LONG).show();
 
-                    deviceArrayList.add(nearDevice);
+                 //   deviceArrayList.add(nearDevice);
                     listAdapter.addDevice(nearDevice);
                     listAdapter.notifyDataSetChanged();
                 }
             }else {
                 if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
 
-                    Log.d(TAG, "ТУТУТУТУТУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУ");
+                    Log.d(TAG, "ACTION_DISCOVERY_FINISHED");
                     if (listAdapter.getCount() == 0) {
-                        Log.d(TAG, "УСТРОЙСТВА НЕ НАЙДЕНЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫ");
+                        Log.d(TAG, "DEVICE_NOT_FOUND");
                         String noDevices = getResources().getText(R.string.none_found).toString();
                         Toast.makeText(getActivity(),
                                 noDevices, Toast.LENGTH_LONG).show();
                     }
-
                 }
             }
         }
@@ -83,13 +77,11 @@ public class bluetoothFragment extends Fragment {
     private void doDiscovery() {
 
         Log.d(TAG, "doDiscovery()");
-        // If we're already discovering, stop it
+
         if (mBtAdapter.isDiscovering()) {
             mBtAdapter.cancelDiscovery();
         }
 
-        System.out.println("ЗДЕСЬ");
-        // Request discover from BluetoothAdapter
         mBtAdapter.startDiscovery();
     }
 
@@ -107,28 +99,6 @@ public class bluetoothFragment extends Fragment {
 
     private void bluetoothConnection() {
 
-
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
-        }
-
-        IntentFilter bluetoothFilter = new IntentFilter();
-        bluetoothFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        bluetoothFilter.addAction(BluetoothDevice.ACTION_FOUND);
-        bluetoothFilter.addAction(BluetoothDevice.ACTION_NAME_CHANGED);
-        bluetoothFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-
-    /*    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        getActivity().registerReceiver(mReceiver, filter);
-
-        IntentFilter filterStart = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-
-      //  filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        getActivity().registerReceiver(mReceiver, filterStart);
-
-        IntentFilter filterStop = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);*/
-        getActivity().registerReceiver(mReceiver, bluetoothFilter);
-
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (mBtAdapter == null) {
@@ -142,36 +112,52 @@ public class bluetoothFragment extends Fragment {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
-        String status;
-        @SuppressLint("HardwareIds") String myDeviceAddress = mBtAdapter.getAddress();
-        String myDeviceName = mBtAdapter.getName();
-        status = myDeviceName + ": " + myDeviceAddress;
-        Device myDevice = new Device(myDeviceName, myDeviceAddress);
-       // deviceArrayList.add(myDevice);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
+        }
 
-        Toast.makeText(getActivity(), status, Toast.LENGTH_LONG).show();
+        //Переделать
+        Long startTime =  java.lang.System.currentTimeMillis();
+        while (!mBtAdapter.isEnabled() || ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                if(  java.lang.System.currentTimeMillis() - startTime > 4000){
+                    return;
+                }
+        }
 
-        pairedDevices = mBtAdapter.getBondedDevices();
+        if(mBtAdapter.isEnabled() && ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            IntentFilter bluetoothFilter = new IntentFilter();
+            bluetoothFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+            bluetoothFilter.addAction(BluetoothDevice.ACTION_FOUND);
+            bluetoothFilter.addAction(BluetoothDevice.ACTION_NAME_CHANGED);
+            bluetoothFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+            getActivity().registerReceiver(mReceiver, bluetoothFilter);
+
+            String status;
+            @SuppressLint("HardwareIds") String myDeviceAddress = mBtAdapter.getAddress();
+            String myDeviceName = mBtAdapter.getName();
+            status = myDeviceName + ": " + myDeviceAddress;
+            Device myDevice = new Device(myDeviceName, myDeviceAddress);
+            // deviceArrayList.add(myDevice);
+
+            Toast.makeText(getActivity(), status, Toast.LENGTH_LONG).show();
+
+            pairedDevices = mBtAdapter.getBondedDevices();
 
 
-        doDiscovery();
+            doDiscovery();
 
+        }
 
     }
-
-
-  /*  @Override
-    public void onDestroy() {
-        super.onDestroy();
-        this.unregisterReceiver(this.mReceiver);
-    }
-    */
-
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
         View view =  inflater.inflate(R.layout.bluetooth_connection,container,false);
         ListView mListView = view.findViewById(R.id.bluetoothListView);
@@ -179,7 +165,6 @@ public class bluetoothFragment extends Fragment {
 
         listAdapter = new BluetoothListAdapter(getActivity(), deviceArrayList);
         mListView.setAdapter(listAdapter);
-
 
         return view;
     }
